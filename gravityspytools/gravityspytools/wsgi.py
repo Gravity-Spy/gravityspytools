b2255e2e2b645259ebd9f5d1b285c1d78a2c9337
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-01-31 11:40:05 -0600

    Made a small mistake

--- gravityspytools/gravityspytools/wsgi.py
+++ gravityspytools/gravityspytools/wsgi.py
@@ -16,7 +16,7 @@ os.environ['HTTPS'] = "on"
 
 _application = get_wsgi_application()
 
-env_variables_to_pass = ['GRAVITYSPY_DATABASE_USER', 'GRAVITYSPY_DATABASE_PASSWD', 'PANOPTES_USERNAME', 'PANOPTES_PASSWORD']
+env_variables_to_pass = ['GRAVITYSPY_DATABASE_USER', 'GRAVITYSPY_DATABASE_PASSWD', 'PANOPTES_USERNAME', 'PANOPTES_PASSWORD', 'PANOPTES_CLIENT_ID', 'PANOPTES_CLIENT_SECRET', 'PANOPTES_PROJECT']
 def application(environ, start_response):
     # pass the WSGI environment variables on through to os.environ
     for var in env_variables_to_pass:
