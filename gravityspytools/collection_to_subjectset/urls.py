dfc8d0d15eea503fda8b425d425e8d9fc8fff13e
Sarah Allen <sarah@zooniverse.org>
2018-02-13 10:44:45 -0600

    Form styling. Fix nav bar active class state

--- gravityspytools/collection_to_subjectset/urls.py
+++ gravityspytools/collection_to_subjectset/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
   
 from . import views
 
+app_name = 'collection_to_subjectset'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^make_subjectset_from_collection/$', views.make_subjectset_from_collection, name='make_subjectset_from_collection'),
--- gravityspytools/display_glitches_DB/urls.py
+++ gravityspytools/display_glitches_DB/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'display_glitches_DB'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^do_DB_search/$', views.do_DB_search, name='do_DB_search'),
