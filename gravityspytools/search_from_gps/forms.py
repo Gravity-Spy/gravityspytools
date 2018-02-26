0b90c3c61c1b2a6cf0c896117a5ca4b614a25b9f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-26 10:32:26 -0600

    Alright I changed the ifo choice to a drop down box

--- gravityspytools/search_from_gps/forms.py
+++ gravityspytools/search_from_gps/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
+STATUS_CHOICES = (
+    ("H1", "H1"), ("L1", "L1"), ("V1", "V1")
+)
 
 class SearchFormGPS(forms.Form):
     howmany = forms.IntegerField(label='How many similar images would you like to return', max_value=200, min_value=1)
     gps = forms.CharField(label = 'GPS time of excess noise source')
-    ifo = forms.CharField(label = 'The interferometer where this exces noise occured')
+    ifo = forms.ChoiceField(choices=STATUS_CHOICES, label = 'The interferometer where this exces noise occured')
