c45ca6246bcfb5540dbbecf281daf2152d91714e
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-08-23 09:09:21 +0100

    Alright the histogram of date times now displays for a collection

---
+++ gravityspytools/collectioninfo/utils.py
@@ -0,0 +1,27 @@
+from matplotlib import use
+use('agg')
+from matplotlib import pyplot
+
+import timeconvert
+import datetime
+from gwpy.time import from_gps
+
+def obtain_figure(SI_glitches):
+    SI_glitches['date'] = SI_glitches['peak_time'].apply(from_gps)
+
+    newestGPS = SI_glitches['date'].max() 
+    oldestGPS = SI_glitches['date'].min()
+
+    bins = (newestGPS - oldestGPS).days/7
+
+    ax = SI_glitches['date'].hist(bins=bins, xrot=45)
+    fig = ax.get_figure()
+
+    ax.set_xlabel("Weeks")
+    ax.set_ylabel("Number of Glitches per Week")
+    ax.set_title("Distribution of Glitches \n Each Bin Represents One Week")
+
+    pyplot.tight_layout()
+
+    #checking and changing xlocs, xlabels
+    return fig
