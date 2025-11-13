aa991c7ab779c4fdc2581eaffd7b5d5b8b8bc205
root <root@gravityspytools.ciera.northwestern.edu>
2025-11-13 10:00:03 -0600

    Current state of gravityspytools

--- gravityspytools/display_glitches_DB/utils.py
+++ gravityspytools/display_glitches_DB/utils.py
@@ -8,7 +8,7 @@ def searchDB(form):
     # process the data in form.cleaned_data as required
     glitchclass = str(form.cleaned_data['glitchclass'])
 
-    SI_glitches = EventTable.fetch('gravityspy', 'trainingset WHERE \"Label\" = \'{0}\''.format(glitchclass), columns = ['gravityspy_id', 'Filename1', 'Filename2', 'Filename3', 'Filename4', 'ifo', 'snr', 'peak_frequency', 'Label']).to_pandas()
+    SI_glitches = EventTable.fetch('gravityspy', 'trainingset WHERE \"Label\" = \'{0}\''.format(glitchclass), columns = ['gravityspy_id', 'Filename1', 'Filename2', 'Filename3', 'Filename4', 'ifo', 'snr', 'peak_frequency', 'Label'], host='gravityspyplus.ciera.northwestern.edu').to_pandas()
 
     SI_glitches['url1'] = SI_glitches[['ifo', 'Filename1']].apply(makelink, axis=1)
     SI_glitches['url2'] = SI_glitches[['ifo', 'Filename2']].apply(makelink, axis=1)
